5ec33d6e77eaa5aac1fd94f121e2d0870db5b844
Fe-Ti <btm.007.qq@gmail.com>
2020-09-23 15:26:18 +0300

    Added new files

--- L-3/Part_1.cpp
+++ L-3/Part_1.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
-#include <experimental/random>
+//#include <experimental/random>
 #include <string>
+#include <ctime>
 
 int main (){
-    int array[10];
+    std::srand(std::time(nullptr));
+    int s_array[10];
+    std::cout << "s_array:" << std::endl;
     for (int i=0;i<10;i++){
-
-        array[i]= std::experimental::randint(-10,10);
-        std::cout << array[i] << " ";
-
+        //s_array[i]= std::experimental::randint(-10,10);
+        s_array[i]= std::rand()%21 - 10;
+        std::cout << s_array[i] << " ";
     }
-    std::cout << std::endl;
+    std::cout << std::endl << "s_array[i]/s_array[9]:" << std::endl;
 
     int summ=0;
     for (int i=0;i<10;i++){
-        std::cout << static_cast<double>(array[i])/array[9] << " ";
-        summ+=array[i];
+        std::cout << static_cast<double>(s_array[i])/s_array[9] << " ";
+        summ+=s_array[i];
     }
-    std::cout << static_cast<double>(summ)/array[9];
+    std::cout << std::endl << "summ(s_array)/s_array[9]:" << static_cast<double>(summ)/s_array[9];
 }
---
+++ L-3/Part_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int main (){
+    unsigned int n=0;
+    std::cout << "Enter n: ";
+    std::cin >> n;
+
+    int* dyn_array = new int [n];
+
+    for (int i=0;i<n;i++){
+        std::cout << "Enter "<<i<<" element: ";
+        std::cin >> dyn_array[i];
+    }
+    for (int i=0;i<n;i++){
+        std::cout << i << " element is " << dyn_array[i] << std::endl;
+    }
+
+    for (int i=0;i<n;i++){
+        if (dyn_array[i]==0){
+            dyn_array[i]=15;
+        }
+        std::cout << i << " element is " << dyn_array[i] << std::endl;
+        }
+
+    int k1=0, k2=0, summ=0;
+    std::cin >> k1 >> k2;
+    if (k1<=k2){
+        for (int i=k1;i<=k2;i++){
+            summ+=dyn_array[i];
+            }
+        }
+    std::cout << summ;
+
+    delete[] dyn_array;
+}
