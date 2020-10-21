d1f9910e3014d892dbf9edf59bb66a26972adb3a
Fe-Ti <btm.007.qq@gmail.com>
2020-10-21 20:53:35 +0300

    Completed 2/3 of LW 5

--- L-5/main.cpp
+++ L-5/main.cpp
@@ -1,13 +1,26 @@
+#include <algorithm>
 #include <iostream>
+#include <map>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
-int main()
+void
+print_vec(const std::vector<int>& vec)
+{
+    for (int i : vec) {
+        std::cout << i << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int
+main()
 {
     std::unordered_multimap<int, std::string> umap = {
-        { 0, "TEST" },
-        { 2, "TEST2" },
-        { 3, "HOMM3" },
+        { 0, "TEST" },     { -2, "TEST2" },     { 3, "HOMMef3" },
+        { 0, "HOMMsef3" }, { -73, "HOMMawd3" }, { -3, "HOMM3sg" },
+        { 3, "TESTwea3" }, { -3, "TESeT3" },    { 5, "TESTs3" },
         { 4, "HOMM4" }
     };
     umap.erase(0);
@@ -16,7 +29,31 @@ int main()
     umap.insert({ 6, "321" });
     umap.insert({ 6, "312" });
     umap.insert({ 6, "132" });
-    for (std::pair<const int, std::string> i : umap) {
-        std::cout << i.first << " " << i.second << std::endl;
+    // for (std::pair<const int, std::string> i : umap) {
+    std::map<int, std::string> simple_map;
+
+    for (auto& i : umap) {
+        simple_map[i.first] = i.second;
+        //        std::cout << i.first << " " << i.second << std::endl;
     }
+    std::cout << std::endl;
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // Part 2
+    //  for (auto& i : simple_map) {
+    //    std::cout << i.first << " " << i.second << std::endl;
+    //  }
+
+    std::cout << "f*ck this task ";
+    std::cout << std::count_if(simple_map.begin(),
+                               simple_map.end(),
+                               [](auto& i) { return i.first < 0; });
+    std::cout << std::endl;
+
+    std::vector<int> output_vec(simple_map.size());
+    std::transform(simple_map.begin(),
+                   simple_map.end(),
+                   output_vec.begin(),
+                   [](std::pair<const int, std::string> i) { return i.first; });
+    print_vec(output_vec);
 }
