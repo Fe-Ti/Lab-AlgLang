14dfccb6bf15a061b8b3820c6ed3739eaa8c5b51
Fedor-Timur B-Kr <btm.007@mail.ru>
2020-12-30 21:47:56 +0300

    Working on L-9('add' and 'print' functions can already do something)

--- L-09/Tree.cpp
+++ L-09/Tree.cpp
@@ -33,6 +33,7 @@
  */
 
 #include <iostream>
+#include <string>
 #include <utility>
 
 template<typename K, typename T> struct Tree_node {
@@ -40,15 +41,16 @@ template<typename K, typename T> struct Tree_node {
     Tree_node<K, T>* parent;
     Tree_node<K, T>* lchild;
     Tree_node<K, T>* rchild;
-    unsigned int tsize;
-    unsigned int theight;
+    unsigned int t_height;
+    unsigned int lst_max_height;
+    unsigned int rst_max_height;
     T data;
 };
 
 template<typename K, typename T> struct Tree {
     Tree_node<K, T>* rootptr;
-    unsigned int tsize;
-    unsigned int theight;
+    unsigned int t_size;
+    // unsigned int t_height;
 };
 
 template<typename K, typename T>
@@ -58,19 +60,14 @@ init_node(K key,
           Tree_node<K, T>* parent = nullptr,
           Tree_node<K, T>* lchild = nullptr,
           Tree_node<K, T>* rchild = nullptr,
-          unsigned int tsize = 1,
-          unsigned int theight = 1)
+          unsigned int rst_max_height = 0,
+          unsigned int lst_max_height = 0,
+          unsigned int t_height = 1)
 {
-    return new Tree_node<K, T>
-    {
-        key,
-        parent,
-        lchild,
-        rchild,
-        tsize,
-        theight,
-        data
-    }
+    return new Tree_node<K, T>{
+        key,      parent,         lchild,         rchild,
+        t_height, lst_max_height, rst_max_height, data
+    };
 }
 
 template<typename K, typename T>
@@ -78,53 +75,134 @@ void
 constructor(Tree<K, T>& birch) // birch is a tree so why not?
 {
     birch.rootptr = nullptr;
-    birch.theight = 0;
-    birch.tsize = 0;
+    birch.t_size = 0;
 }
 
 template<typename K, typename T>
-void
-leftsmall_turn()
-{
-}
-
-template<typename K, typename T>
-void
-rightsmall_turn()
+bool
+wants_lsmt(Tree_node<K, T>*& branch)
 {
 }
 
 template<typename K, typename T>
+bool
+wants_rsmt(Tree_node<K, T>*& branch)
+{
+}
+
+template<typename K, typename T>
+bool
+wants_lbt(Tree_node<K, T>*& branch)
+{
+}
+
+template<typename K, typename T>
+bool
+wants_rbt(Tree_node<K, T>*& branch)
+{
+}
+
+template<typename K, typename T>
+void // prototyping
+left_small_turn(Tree_node<K, T>*& branch)
+{
+    Tree_node<K, T>*;
+    if(branch->parent->lchild == branch) // if branch is lchild of parent then
+        branch->parent->lchild = branch->rchild; // change l parent's pointer
+    else                                         // else
+        branch->parent->rchild = branch->rchild; // change right one
+
+    // branch->lchild is kept in place
+    branch->rchild->parent = branch->parent;
+    branch->parent = branch->rchild;
+    branch->rchild->lchild->parent = branch;
+}
+
+template<typename K, typename T>
 void
-leftbig_turn()
+right_small_turn()
 {
 }
 
 template<typename K, typename T>
 void
-rightbig_turn()
+left_big_turn()
 {
+}
+
+template<typename K, typename T>
+void
+right_big_turn()
+{
+}
+
+// internal functions
+template<typename K, typename T>
+void
+update_lstmh(Tree_node<K, T>*& branch, Tree_node<K, T>*& new_node)
+{
+    if(new_node->t_height > branch->lst_max_height)
+        branch->lst_max_height = new_node->t_height;
 }
 template<typename K, typename T>
 void
-add(Tree_node<R, T>& branch, K key, T data, bool check_enable = true)
+update_rstmh(Tree_node<K, T>*& branch, Tree_node<K, T>*& new_node)
 {
+    if(new_node->t_height > branch->rst_max_height)
+        branch->rst_max_height = new_node->t_height;
+}
+
+template<typename K, typename T>
+void
+grow(Tree_node<K, T>*& branch,
+     Tree_node<K, T>*& new_node,
+     bool check_enable = true)
+{
+    if(branch == nullptr) {
+        branch = new_node;
+    } else {
+        new_node->parent = branch;
+        ++new_node->t_height;
+        if(branch->key > new_node->key) {
+            grow(branch->lchild, new_node, check_enable);
+            update_lstmh(branch, new_node);
+        } else {
+            grow(branch->rchild, new_node, check_enable);
+            update_rstmh(branch, new_node);
+        }
+        if(check_enable) {
+            if(wants_lsmt(branch)) {
+                left_small_turn();
+            }
+            if(wants_rsmt(branch)) {
+                right_small_turn();
+            }
+            if(wants_lbt(branch)) {
+                left_big_turn();
+            }
+            if(wants_rbt(branch)) {
+                right_big_turn();
+            }
+        }
+    }
 }
 
 template<typename K, typename T>
 void
 add(Tree<K, T>& birch, K key, T data, bool check_enable = true)
 {
+    Tree_node<K, T>* new_node = init_node(key, data);
     if(birch.rootptr != nullptr) {
-
+        grow(birch.rootptr, new_node, check_enable);
     } else {
-        birch.rootptr = init_node(key, data);
+        birch.rootptr = new_node;
     }
+    std::cout << "Added " << data << " with key " << key << std::endl;
 }
 
 template<typename K, typename T>
 void
-add(Tree<K, T>& birch, pair<K, T> tuple) // an overload for pairs
+add(Tree<K, T>& birch, std::pair<K, T> tuple) // an overload for pairs
 {
     add(birch, tuple.first, tuple.second); // simply parse the pair
 }
@@ -137,6 +215,34 @@ remove()
 
 template<typename K, typename T>
 void
+recuprint(Tree_node<K, T>*& branch, std::string& recuheader)
+{
+    std::cout << recuheader << branch->data << std::endl;
+    if(!recuheader.empty())
+        recuheader.erase(recuheader.size() - 7, 7);
+    recuheader.push_back('|');
+    if(branch->lchild != nullptr) {
+        recuheader += "LChild ";
+        recuprint(branch->lchild, recuheader);
+    }
+    if(branch->rchild != nullptr) {
+        recuheader += "RChild ";
+        recuprint(branch->rchild, recuheader);
+    }
+    recuheader.pop_back();
+}
+
+template<typename K, typename T>
+void
+print(Tree<K, T>& birch)
+{
+    std::string recuheader;
+    std::cout << "AVeryLittle Tree:" << std::endl << "Root ";
+    recuprint(birch.rootptr, recuheader);
+}
+
+template<typename K, typename T>
+void
 destructor()
 {
 }
@@ -144,5 +250,18 @@ destructor()
 int
 main()
 {
+    Tree<int, std::string> tester;
+    constructor(tester);
+    add(tester, 5, std::string("5"));
+    add(tester, 4, std::string("4"));
+    add(tester, 2, std::string("2"));
+    add(tester, 3, std::string("3"));
+    add(tester, 1, std::string("1"));
+    add(tester, 6, std::string("6"));
+    add(tester, 8, std::string("8"));
+    add(tester, 7, std::string("7"));
+    add(tester, 234, std::string("234"));
+    add(tester, 2347, std::string("2347"));
+    print(tester);
     return 0;
 }
